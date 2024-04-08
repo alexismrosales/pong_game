78fa11b57b937816a21089f910ddd4ce19f7ef8b
rarksz <aleisjorge91@gmail.com>
2024-04-08 00:25:15 -0600

    view by status updated

--- frontend/src/components/gameStart/index.tsx
+++ frontend/src/components/gameStart/index.tsx
@@ -1,5 +1,7 @@
-import { FunctionComponent, useState } from "react"
-import { connect } from "tls"
+
+import { FunctionComponent, useEffect, useState } from "react"
+
+
 import style from "./styles.module.css"
 
 interface GameStartProps {
@@ -9,35 +11,64 @@ interface GameStartProps {
 interface Connection {
   connectToggle: () => void
   newGameToggle: () => void
+  start: () => void
   status: "disconnected" | "connecting" | "connected"
 }
 
 const GameStart: React.FC<GameStartProps> = ({ onStartToggle }) => {
-  const [start, setStart] = useState(false);
+  const [connect, setConnect] = useState(false);
+  const [newGame, setNewGame] = useState(false);
+  const handleConnect = () => {
+    setConnect(!connect);
+  }
+  const handleNewGame = () => {
+    setNewGame(!newGame);
+  }
+
   return (
     <div id="GameStart" className={style.container}>
       <div className="flex flex-col">
-
-        <h1 className="text-white">Hello from gamestart </h1>
-        <br />
-        <button className="text-white" onClick={onStartToggle}>Start Game</button>
+        <TitleScreen
+          connectToggle={handleConnect}
+          newGameToggle={handleNewGame}
+          start={onStartToggle}
+          status="disconnected" />
       </div>
     </div>
   )
 }
-const ConnectSession: FunctionComponent<Connection> = props => {
+const TitleScreen: FunctionComponent<Connection> = props => {
   const status = props.status
+  const [counter, setCounter] = useState(3);
+
+  useEffect(() => {
+    if (status === "connected") {
+      const intervalId = setTimeout(() => {
+        if (counter > 0) {
+          setCounter(counter - 1);
+        } else {
+          clearTimeout(intervalId);
+          props.start();
+        }
+      }, 1500);
+      return () => {
+        clearInterval(intervalId);
+      }
+    }
+  }, [counter])
+
   if (status === "disconnected") {
     return (
-      <>
+      <div className="text-center">
         <h1>PONG GAME</h1>
         <form>
           <input type="text" placeholder="Type the code of the session" />
+          <br />
           <button onClick={props.connectToggle}>Connect</button>
           <p>or</p>
           <button onClick={props.newGameToggle}>Create new game</button>
         </form>
-      </>
+      </div>
     )
   } else if (status === "connecting") {
     return (
@@ -48,6 +79,8 @@ const ConnectSession: FunctionComponent<Connection> = props => {
   }
   return (
     <>
+      <p>Starting in </p>
+      <span>{counter}</span>
       <p>Connection Succesfully</p>
     </>
   )
