323c098cf839144fd830758ead8eb2f10089b5e0
rarksz <aleisjorge91@gmail.com>
2024-04-27 02:03:26 -0600

    Updated ball colission

--- frontend/src/components/game/status.ts
+++ frontend/src/components/game/status.ts
@@ -1,21 +1,19 @@
 export const isOnPaddle =
-  (playerPos: number,
-    ballPos: { x: number, y: number },
-    paddleHeight: number) => {
+  (playerPos: number, ballPos: { x: number, y: number }, paddleHeight: number) => {
+    let status;
+    let pos;
     // A colission of the paddle and the ball happens when ballPos.y belongs to the interval [playerPos, playerPos + paddleHeight]
     // it means that the ball will be colissioning with the paddle
     const paddleSize = playerPos + paddleHeight;
     // 30 is the position where ball will dettect the colission
-    if (ballPos.x === 30 && !(ballPos.y >= playerPos && ballPos.y + 25 <= paddleSize)) {
-      return { onPaddle: false, pos: -1 };
-    } else if (ballPos.x == 30) {
-      const ballHeight = ballPos.y + 25;
-      const topPos = abs();
-      return { onPaddle: true, };
+    if (ballPos.x === 30) {
+      if (ballPos.y >= playerPos && ballPos.y <= paddleSize) {
+        status = true;
+        pos = playerPos;
+      } else {
+        status = false;
+        pos = -1;
+      }
     }
-    return null;
+    return { status: status, pos: pos }
   }
-//:playerPos = 0
-// 
-//--------
-//h - 
--- frontend/src/tests/game/status.test.ts
+++ frontend/src/tests/game/status.test.ts
@@ -7,6 +7,42 @@ describe("isOnPaddle", () => {
     const playerPos = 0;
     const paddleHeight = 10;
     const result = isOnPaddle(playerPos, ballPos, paddleHeight);
-    expect(result).toEqual(true);
+    expect(result.status).toEqual(true);
   });
 });
+describe("isOnLimitPaddle", () => {
+  it("Should return true if the position of the ball is at limit of paddle", () => {
+    // Ball will be in coordinates (30, 0); 
+    const ballPos = { x: 30, y: 10 };
+    // Playerpos will be only axis y 
+    const playerPos = 0;
+    const paddleHeight = 10;
+    const result = isOnPaddle(playerPos, ballPos, paddleHeight);
+    expect(result.status).toEqual(true);
+  });
+});
+describe("isOnLimitPaddle2", () => {
+  it("Should return true if the position of the ball is at limit of paddle", () => {
+    // Ball will be in coordinates (30, 0); 
+    const ballPos = { x: 30, y: 20 };
+    // Playerpos will be only axis y 
+    const playerPos = 10;
+    const paddleHeight = 10;
+    const result = isOnPaddle(playerPos, ballPos, paddleHeight);
+    expect(result.status).toEqual(true);
+  });
+});
+
+describe("isOutOfPaddle", () => {
+  it("Should return false if the position of the ball is not in the range of the size of the paddle", () => {
+    // Ball will be in coordinates (30, 0); 
+    const ballPos = { x: 30, y: 30 };
+    // Playerpos will be only axis y 
+    const playerPos = 15;
+    const paddleHeight = 10;
+    const result = isOnPaddle(playerPos, ballPos, paddleHeight);
+    expect(result.status).toEqual(false);
+  });
+});
+
+
